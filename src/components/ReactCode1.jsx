eca88e8e2d500b6addd556eb8de29d00dc5e7ce5
frontend-mahdi <mahdi.313.ab@gmail.com>
2022-11-20 13:47:11 +0330

    template challenges created 1

--- src/components/ReactCode1.jsx
+++ src/components/ReactCode1.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import react1 from "./../img/react1.png";
+import react4 from "./../img/react4.png";
 const ReactCode1 = () => {
   return (
     <div>
       <center>
         <img src={react1} alt="" />
+      </center>
+      <center>
+        <img src={react4} alt="" />
       </center>
     </div>
   );
