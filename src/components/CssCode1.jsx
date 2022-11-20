87ae608ee652ddab08b49b9f7bedb033e70950de
mohammadT9030 <torabi9030@gmail.com>
2022-11-20 14:27:25 +0330

    inital commit

--- src/components/CssCode1.jsx
+++ src/components/CssCode1.jsx
@@ -1,10 +1,28 @@
 import React from "react";
 import css1 from "./../img/css1.png";
+
+const styles = {
+  
+};
+
 const CssCode1 = () => {
+  const mystyle = {
+    color: "white",
+    backgroundColor: "DodgerBlue",
+    padding: "10px",
+    fontFamily: "Arial"
+  };
   return (
     <div>
       <center>
         <img src={css1} alt="" />
+        <div className="container">
+          <h1 style={styles.section}>hello world</h1>
+          <div className="image-box"></div>
+          <div className="image-box"></div>
+          <div className="image-box"></div>
+          <div className="image-box"></div>
+        </div>
       </center>
     </div>
   );
