0def1378df6328d099cbe86c2b7a0918a1385354
mohammadT9030 <torabi9030@gmail.com>
2022-11-20 16:18:46 +0330

    react 2

--- src/components/ReactCode2.jsx
+++ src/components/ReactCode2.jsx
@@ -5,6 +5,15 @@ const ReactCode2 = () => {
     <div>
       <center>
         <img src={react2} alt="" />
+        <div className="container">
+          <form action="#" className="login-form">
+            <label htmlFor="username">نام کاربری</label>
+            <input type="text" name="username" />
+            <label htmlFor="username">رمز عبور</label>
+            <input type="text" name="password" />
+            <button>ورود</button>
+          </form>
+        </div>
       </center>
     </div>
   );
